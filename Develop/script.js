c14dd70ecd22edcf2e902cc7bc4e0777c47a07c8
seantaylor1006 <seant1006@gmail.com>
2023-10-04 19:26:41 -0400

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+let currentDay = dayjs();
+$('#currentDay').text(today.format(MM))
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -8,6 +10,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
